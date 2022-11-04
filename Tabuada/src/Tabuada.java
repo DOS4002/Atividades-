e939cd3babb68471c4c3c5b8ad62636f500077b3
DOS4002 <113701704+DOS4002@users.noreply.github.com>
2022-11-03 23:33:13 -0300

    Add files via upload

---
+++ Tabuada/src/Tabuada.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Tabuada {
+	public static void main(String[] algs) {
+		Scanner in = new Scanner(System.in);
+		int i, n, res;
+		
+		System.out.print("Digite um número: ");
+		n = in.nextInt();
+		System.out.println();		
+		for(i = 1; i <= 10; i++){
+			res = n * i;
+			System.out.println(n+" x "+i+ " = "+ res);
+		}
+		in.close();
+	}
+
+}
